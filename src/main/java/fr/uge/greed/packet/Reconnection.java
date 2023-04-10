0f885da4469b06f9ac4a0b2fa69caf7081d1a004
SebastienDS <47292880+SebastienDS@users.noreply.github.com>
2023-04-10 21:04:24 +0200

    Fix Reconnection packet: add missing information

--- src/main/java/fr/uge/greed/packet/Reconnection.java
+++ src/main/java/fr/uge/greed/packet/Reconnection.java
@@ -3,25 +3,28 @@ package fr.uge.greed.packet;
 import fr.uge.greed.Payload;
 import fr.uge.greed.SocketAddress;
 
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Objects;
 
-public record Reconnection(List<SocketAddress> addresses) implements Payload {
+public record Reconnection(SocketAddress address, List<SocketAddress> addresses) implements Payload {
   public static final byte OPCODE = 11;
 
   public Reconnection {
+    Objects.requireNonNull(address);
     Objects.requireNonNull(addresses);
     addresses = List.copyOf(addresses);
   }
   @Override
   public int getRequiredBytes() {
-    return Integer.BYTES + addresses.stream().mapToInt(SocketAddress::getRequiredBytes).sum();
+    return address.getRequiredBytes() + Integer.BYTES + addresses.stream().mapToInt(SocketAddress::getRequiredBytes).sum();
   }
 
   @Override
   public void encode(ByteBuffer buffer) {
     Objects.requireNonNull(buffer);
+    address.encode(buffer);
     buffer.putInt(addresses.size());
     addresses.forEach(address -> address.encode(buffer));
   }
