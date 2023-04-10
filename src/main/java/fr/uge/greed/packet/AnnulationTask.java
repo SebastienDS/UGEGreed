3f38be2621f4044033846cbca7bc64f346d56bd3
SebastienDS <47292880+SebastienDS@users.noreply.github.com>
2023-04-10 13:36:10 +0200

    Fix AnnulationTask

--- src/main/java/fr/uge/greed/packet/AnnulationTask.java
+++ src/main/java/fr/uge/greed/packet/AnnulationTask.java
@@ -10,15 +10,20 @@ public record AnnulationTask(long id, byte status, long startRemainingValues) im
   public static final byte CANCEL_MY_TASK = 0;
   public static final byte CANCEL_ASSIGNED_TASK = 1;
 
+  public AnnulationTask {
+    if (status < 0 || status > 1) {
+      throw new IllegalArgumentException();
+    }
+  }
+
   @Override
   public int getRequiredBytes() {
-    return 2 * Long.BYTES;
+    return 2 * Long.BYTES + Byte.BYTES;
   }
 
   @Override
   public void encode(ByteBuffer buffer) {
     Objects.requireNonNull(buffer);
-    buffer.putLong(id).putLong(startRemainingValues);
+    buffer.putLong(id).put(status).putLong(startRemainingValues);
   }
-
 }
